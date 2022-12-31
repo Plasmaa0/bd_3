b3d0c3950fa2e6613ab44915d90739112c7ffbe6
plasmaa0 <plasmaa0@gmail.com>
2022-12-31 04:35:01 +0300

    feat: theme switcher fix: finally working 100vh layout

--- react_app/src/Pages/Global/App.tsx
+++ react_app/src/Pages/Global/App.tsx
@@ -11,11 +11,14 @@ import {Navbar} from "./Navbar";
 import {UserPage} from "../UserPage/UserPage";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {FileView} from "../FileView/FileView";
-import {Layout, Typography} from "antd";
+import {ConfigProvider, Layout, theme, Typography} from "antd";
 import {SearchForm} from "../SearchPage/SearchForm";
 import {ClassTreeSearch} from "../ClassTree/ClassTreeSearch";
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
 import {GetToken, GetUser} from "../../Functions/DataStoring";
+import {isDarkTheme, ThemeSwitcher} from "./ThemeSwitcher";
+
+const {darkAlgorithm, compactAlgorithm, defaultAlgorithm} = theme;
 
 export function App() {
     const queryClient = new QueryClient({
@@ -28,61 +31,70 @@ export function App() {
     })
     const token: string = GetToken();
     const user: string = GetUser();
+    const [isDarkThemeState, setIsDarkThemeState] = useState(isDarkTheme());
     const [collapsed, setCollapsed] = useState(false);
     // return (<TestPage/>); //fixme delete this
     if (!token || !user) {
         return (
-            <Layout className="box">
-                <QueryClientProvider client={queryClient}>
-                    <BrowserRouter>
-                        <Layout.Content className="row content">
-                            <Routes>
-                                <Route path="/register" element={<Register/>}/>
-                                <Route path="*"
-                                       element={<Login/>}/>
-                            </Routes>
-                        </Layout.Content>
-                        <Layout.Footer className="row footer">
-                            <Typography.Text>Top 1 file server of the world</Typography.Text>
-                        </Layout.Footer>
-                    </BrowserRouter>
-                </QueryClientProvider>
-            </Layout>
+            <ConfigProvider theme={{algorithm: isDarkTheme() ? darkAlgorithm : defaultAlgorithm}}>
+                <Layout className="box" style={{minHeight:"100vh"}}>
+                    <QueryClientProvider client={queryClient}>
+                        <BrowserRouter>
+                            <Layout.Content className="row content">
+                                <Routes>
+                                    <Route path="/register" element={<Register/>}/>
+                                    <Route path="*"
+                                           element={<Login/>}/>
+                                </Routes>
+                                <ThemeSwitcher isDarkTheme={isDarkTheme()}
+                                               setIsDarkTheme={(isDarkTheme) => setIsDarkThemeState(isDarkTheme)}/>
+                            </Layout.Content>
+                            <Layout.Footer className="row footer">
+                                <Typography.Text>Top 1 file server of the world</Typography.Text>
+                            </Layout.Footer>
+                        </BrowserRouter>
+                    </QueryClientProvider>
+                </Layout>
+            </ConfigProvider>
         )
     }
     return (
-        <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
-                <Layout className="box">
-                    <Layout.Sider collapsible breakpoint="lg" trigger={null} collapsed={collapsed}
-                                  onCollapse={(value) => setCollapsed(value)} className="row header">
-                        <Navbar collapsed={collapsed} setCollapsed={setCollapsed}/>
-                    </Layout.Sider>
-                    <Layout>
-                        <Layout.Content className="row content">
-                            <Routes>
-                                <Route path="/" element={<Home/>}/>
-                                <Route path="/login"
-                                       element={<Login/>}/>
-                                <Route path="/register" element={<Register/>}/>
-                                <Route path="/class-tree"
-                                       element={<ClassTreeSearch/>}/>
-                                <Route path="/search"
-                                       element={<SearchForm/>}/>
-                                <Route path="/:user" element={<UserPage/>}/>
-                                <Route path="/:user/:project_path/*"
-                                       element={<ProjectPage/>}/>
-                                <Route path="/file/:user/*" element={<FileView/>}/>
-                                <Route path='*' element={<Page404/>}/>
-                            </Routes>
-                        </Layout.Content>
-                        <Layout.Footer className="row footer">
-                            <Typography.Text>Top 1 file server of the world</Typography.Text>
-                        </Layout.Footer>
+        <ConfigProvider theme={{algorithm: isDarkTheme() ? darkAlgorithm : defaultAlgorithm}}>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <Layout className="box" style={{minHeight:"100vh"}}>
+                        <Layout.Sider collapsible breakpoint="lg" trigger={null} collapsed={collapsed}
+                                      onCollapse={(value) => setCollapsed(value)} className="row header">
+                            <ThemeSwitcher isDarkTheme={isDarkTheme()}
+                                           setIsDarkTheme={(isDarkTheme) => setIsDarkThemeState(isDarkTheme)}/>
+                            <Navbar collapsed={collapsed} setCollapsed={setCollapsed}/>
+                        </Layout.Sider>
+                        <Layout>
+                            <Layout.Content className="row content">
+                                <Routes>
+                                    <Route path="/" element={<Home/>}/>
+                                    <Route path="/login"
+                                           element={<Login/>}/>
+                                    <Route path="/register" element={<Register/>}/>
+                                    <Route path="/class-tree"
+                                           element={<ClassTreeSearch/>}/>
+                                    <Route path="/search"
+                                           element={<SearchForm/>}/>
+                                    <Route path="/:user" element={<UserPage/>}/>
+                                    <Route path="/:user/:project_path/*"
+                                           element={<ProjectPage/>}/>
+                                    <Route path="/file/:user/*" element={<FileView/>}/>
+                                    <Route path='*' element={<Page404/>}/>
+                                </Routes>
+                            </Layout.Content>
+                            <Layout.Footer className="row footer">
+                                <Typography.Text>Top 1 file server of the world</Typography.Text>
+                            </Layout.Footer>
+                        </Layout>
                     </Layout>
-                </Layout>
-            </BrowserRouter>
-            <ReactQueryDevtools initialIsOpen={true}/>
-        </QueryClientProvider>
+                </BrowserRouter>
+                <ReactQueryDevtools initialIsOpen={true}/>
+            </QueryClientProvider>
+        </ConfigProvider>
     );
 }
