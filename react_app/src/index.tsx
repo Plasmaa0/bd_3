04181b28b76727371028379a6cabba5747d0a76f
plasmaa0 <plasmaa0@gmail.com>
2022-12-31 04:40:53 +0300

    refactor: moved ConfigProvider to App.tsx for theme switcher

--- react_app/src/index.tsx
+++ react_app/src/index.tsx
@@ -3,43 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import {App} from "./Pages/Global/App";
 import reportWebVitals from './other/reportWebVitals';
-import {ConfigProvider, theme} from "antd";
 import {CookiesProvider} from "react-cookie";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-const {darkAlgorithm, compactAlgorithm, defaultAlgorithm} = theme;
-
 root.render(
     <React.StrictMode>
-        <ConfigProvider theme={{
-            token: {
-                // colorPrimary: '#37b08f',
-                // colorPrimaryBg: '#1c463d',
-                // colorSuccess: '#06f585',
-                // colorWarning: '#f37e09',
-                // colorError: '#ff0005',
-                // colorInfo: '#3c5180',
-                // colorBgBase: '#d2d6e3',
-                // colorTextBase: '#33394d',
-                // colorText: '#0e0842',
-                // colorTextSecondary: '#6f92e8',
-                // colorTextPlaceholder: '#6f92e8',
-                // colorTextDisabled: '#4d669f',
-                // colorTextHeading: '#62ffd0',
-                // colorTextLabel: '#4ac0ff',
-                // colorTextDescription: '#67b6ee',
-                // colorTextLightSolid: '#8d94de',
-                fontSize: 20
-            },
-            algorithm: [defaultAlgorithm, compactAlgorithm],
-        }}>
-            <CookiesProvider>
-                <App/>
-            </CookiesProvider>
-        </ConfigProvider>
+        <CookiesProvider>
+            <App/>
+        </CookiesProvider>
     </React.StrictMode>
 );
 
